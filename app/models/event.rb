c3e09d83763302ba2415af80bdea20bf20df80b5
Passing01 <ouerahim456@gmail.com>
2024-07-31 20:52:37 +0000

    add file

--- app/models/event.rb
+++ app/models/event.rb
@@ -13,4 +13,15 @@ class Event < ApplicationRecord
   #def is_free?
   #  price == 0
   #end
+
+  belongs_to :user
+
+  validates :start_date, presence: true
+  validates :duration, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :location, presence: true
+
+
 end
